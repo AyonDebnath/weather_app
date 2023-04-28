605a0906f8660148eb00661b4e1f7dac085169f6
AyonDebnath <ayonnath.31@gmail.com>
2023-04-28 18:03:08 -0230

    Add the input statements

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,8 +5,16 @@ function Weather() {
     return (
         <div className = "weather">
             <span className = "title"> Weather App </span>
+            <br/>
+        <form>
+            <input type = "text" name = "city" placeholder="city"/>
+            <input type = "text" name = "country" placeholder="country"/>
+            <button className="getweather">Submit</button>
+        </form>
         </div>
     )
+
+    
 }
 
 export default Weather
