9b3cee8b5d96e8942b9a06a67564f5ba14084cc8
AyonDebnath <ayonnath.31@gmail.com>
2023-04-29 11:06:30 -0230

    Fix the inputs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 
 function Weather() {
+
+    const [form, setForm] = useState({
+        city:"", 
+        country:""
+    })
+    const handleChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value
+        if(name == "city") {
+            setForm({...form, city:value})
+        }
+        if(name == "country") {
+            setForm({...form, country:value})
+        }
+        console.log(form.city, form.country)
+    }
     return (
         <div className = "weather">
             <span className = "title"> Weather App </span>
             <br/>
         <form>
-            <input type = "text" name = "city" placeholder="city"/>
+            <input type = "text" name = "city" placeholder="city" onChange={e => handleChange(e)}/>
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <input type = "text" name = "country" placeholder="country"/>
+            <input type = "text" name = "country" placeholder="country" onChange={e => handleChange(e)}/>
             <button className="getweather">Submit</button>
         </form>
         </div>
